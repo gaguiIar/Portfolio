b593eaedc1117554a7c2937ab905a1f5203e2e23
g <gfaguila@usc.edu>
2022-07-17 03:35:31 -0700

    updated coming soon page, changed hover effect, changed bg color

--- comingSoon.html/script.js
+++ comingSoon.html/script.js
@@ -7,33 +7,17 @@ function fetchData() {
       return response.json();
     })
     .then(data => {
-      console.log(data.recenttracks.track);
       const dataArray = data.recenttracks.track.map(user => {
           return `${user.name}:${user.artist['#text']}:${user.image['3']['#text']}`
         })
         .join("");
-      console.log(dataArray);
 
       let parsed = dataArray;
       const newArray = parsed.split(":");
       let song = newArray[0];
       let artist = newArray[1];
       let artwork = `<img id = "musPhoto"src="${newArray[3]}" alt="">`;
-
-
-      console.log(song);
-      console.log(artist);
-      console.log(artwork);
-
       const newMsg = `${song} by ${artist}`;
-
-
-
-
-
-
-
-
 
       document.querySelector(".photo").insertAdjacentHTML("afterbegin", artwork);
       document.querySelector("#artistSong").insertAdjacentHTML("afterbegin", newMsg);
